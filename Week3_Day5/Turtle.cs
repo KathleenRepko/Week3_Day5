15c8d950a25ce53b90d47b2e7288a8db894bc694
KathleenRepko <kathleen.repko@gmail.com>
2017-10-13 10:18:41 -0400

    Completed Static example.

---
+++ Week3_Day5/Turtle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3_Day5
+{
+    class Turtle
+    {
+        //This static variable belongs to the CLASS.
+        //It is shared among all instances.
+        //This also means we can use the variable without creating instances first.
+        static int turtleCount;
+
+        public Turtle()
+        {
+            turtleCount++;
+        }
+        
+        //This static method belongs to the class.
+        //It's shared among all instances.
+        //It means we can use the method without creating any instances first.
+        public static void WarCry()
+        {
+            Console.WriteLine("We are an army of " + turtleCount + " turtles!");
+        }
+
+    }
+}
